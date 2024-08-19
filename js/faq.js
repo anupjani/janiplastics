e5a1527bf5487acd0c057546b23f07ded17324cc
anupjani <126309524+anupjani@users.noreply.github.com>
2024-08-19 23:48:02 +1000

    Add files via upload

---
+++ js/faq.js
@@ -0,0 +1,55 @@
+/**
+ * This is angular JS code. 
+ */
+			var faqApp = angular.module('faqMdl',[]);
+			faqApp.controller('faqCtrl', function ($scope) {
+				$scope.showAll = function() {
+					$scope.faq1 = true;
+					$scope.faq2 = true;
+					$scope.faq3 = true;
+					$scope.faq4 = true;
+					$scope.faq5 = true;
+					$scope.faq6 = true;
+					$scope.faq7 = true;
+					$scope.faq8 = true;
+					$scope.faq9 = true;
+					$scope.faq10 = true;
+					$scope.faq11 = true;
+					$scope.faq12 = true;
+					$scope.faq13 = true;
+					$scope.faq14 = true;
+					$scope.faq15 = true;
+					$scope.faq16 = true;
+					$scope.faq17 = true;
+					$scope.faq18 = true;
+					$scope.faq19 = true;
+					$scope.faq20 = true;
+					$scope.faq21 = true;
+					$scope.faq22 = true;
+				}
+				$scope.hideAll = function() {
+					$scope.faq1 = false;
+					$scope.faq2 = false;
+					$scope.faq3 = false;
+					$scope.faq4 = false;
+					$scope.faq5 = false;
+					$scope.faq6 = false;
+					$scope.faq7 = false;
+					$scope.faq8 = false;
+					$scope.faq9 = false;
+					$scope.faq10 = false;
+					$scope.faq11 = false;
+					$scope.faq12 = false;
+					$scope.faq13 = false;
+					$scope.faq14 = false;
+					$scope.faq15 = false;
+					$scope.faq16 = false;
+					$scope.faq17 = false;
+					$scope.faq18 = false;
+					$scope.faq19 = false;
+					$scope.faq20 = false;
+					$scope.faq21 = false;
+					$scope.faq22 = false;
+				}
+			}
+			);
